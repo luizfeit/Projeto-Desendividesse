3e7fd481f963740c9ca4ee91f0ae5a03f66b62f1
luizfeit <luiz.felipe301@hotmail.com>
2021-10-22 17:15:26 -0300

    Projeto Desendividisse

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { Login } from './src/components/Login';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Login />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
