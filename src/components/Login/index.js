a339f3517fc089a72c5dc99d2a3afe0063d16e9e
luizfeit <luiz.felipe301@hotmail.com>
2021-11-23 23:15:41 -0300

    Tela de login funcionando

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -16,12 +16,12 @@ export function Login({ navigation }) {
 
     const ValidarLogin = (email, senha) => {
         Usuarios.findUser(email, senha)
-            .then(navigation.navigate('Inicial'))
+            .then(() => navigation.navigate("Inicial"))
             .catch(err => alert(err))
     }
     const printUser = (user) => {
         console.log(`id_usuario:${user.id_usuario}, nome:${user.nome}, email:${user.email}, senha:${user.senha}`)
-      }
+    }
 
     return (
         <PaperProvider theme={theme} >
@@ -40,14 +40,14 @@ export function Login({ navigation }) {
                                 style={styles.cardInput}
                                 keyboardType='email-address'
                                 value={email}
-                                onChangeText={email => setEmail(email)}
+                                onChangeText={email => setEmail(email || null)}
                             />
 
                             <TextInput label="Senha"
                                 style={styles.cardInput}
                                 secureTextEntry={true}
                                 value={senha}
-                                onChangeText={senha => setSenha(senha)} />
+                                onChangeText={senha => setSenha(senha || null)} />
 
                             <Button uppercase={false} style={styles.cardButton} color='#000'
                             // onPress={() => Usuarios.create({nome:'Matheus', email:'matheus@', senha:'1234' })
@@ -58,17 +58,17 @@ export function Login({ navigation }) {
                             </Button>
                             <Button mode='contained' style={styles.cardLogin}
                                 onPress={() => ValidarLogin(email, senha)}
-                                >
+                            >
                                 Login
                             </Button>
 
-                            <Button uppercase={false} style={styles.cardButton} color='#000' 
-                            onPress={() => navigation.navigate('Cadastro')}
+                            <Button uppercase={false} style={styles.cardButton} color='#000'
+                                onPress={() => navigation.navigate('Cadastro')}
                             // onPress={() => Usuarios.find(1)
                             //     .then(user => printUser(user))
                             //     .catch(err => console.log(err))
-    // }
-                             >
+                            // }
+                            >
                                 Cadastre-se
                             </Button>
 
